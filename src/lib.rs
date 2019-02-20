025f141017c4e9cc5926223aa8adaa26f8711376
Scott McMurray <scottmcm@users.noreply.github.com>
2019-02-19 20:27:19 -0800

    Add generate; move all impls to being the same macro

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,12 @@ pub trait ArrayTools: Sized + Sealed {
     fn from_tuple(tuple: Self::Tuple) -> Self;
     fn into_tuple(self) -> Self::Tuple;
 
+    fn generate<F>(f: F) -> Self
+        where Self: ArrayGenerate<F>
+    {
+        ArrayGenerate::generate(f)
+    }
+
     fn map<T, F>(self, f: F) -> <Self as ArrayMap<T, F>>::Output
         where Self: ArrayMap<T, F>
     {
@@ -57,6 +63,10 @@ pub trait ArrayTools: Sized + Sealed {
 mod traits {
     pub trait Sealed {}
 
+    pub trait ArrayGenerate<F> {
+        fn generate(f: F) -> Self;
+    }
+
     pub trait ArrayMap<T, F> {
         type Output;
         fn map(array: Self, f: F) -> Self::Output;
@@ -85,121 +95,15 @@ mod traits {
     }
 }
 
+#[allow(unused_mut, unused_variables)]
 mod impls {
     use super::*;
 
-    impl<T> Sealed for [T; 0] {}
-    impl<T> ArrayTools for [T; 0] {
-        type Element = T;
-        const LEN: usize = 0;
-        fn as_slice(&self) -> &[Self::Element] { self }
-        fn as_mut_slice(&mut self) -> &mut [Self::Element] { self }
-
-        type Tuple = ();
-        fn from_tuple(_tuple: Self::Tuple) -> Self { [] }
-        fn into_tuple(self) -> Self::Tuple { () }
-    }
-    impl<T, U, F> ArrayMap<U, F> for [T; 0] {
-        type Output = [U; 0];
-        fn map(_array: Self, _f: F) -> Self::Output { [] }
-    }
-    impl<'a, T> ArrayAsRef for &'a [T; 0]
-    {
-        type Output = [&'a T; 0];
-        fn as_ref(_array: Self) -> Self::Output { [] }
-    }
-    impl<'a, T> ArrayAsMut for &'a mut [T; 0]
-    {
-        type Output = [&'a mut T; 0];
-        fn as_mut(_array: Self) -> Self::Output { [] }
-    }
-    impl<T> ArrayPush<T> for [T; 0] {
-        type Output = [T; 1];
-        fn push_back(_array: Self, item: T) -> Self::Output { [item] }
-        fn push_front(_array: Self, item: T) -> Self::Output { [item] }
-    }
-    impl<T> ArrayPop<T> for [T; 1] {
-        type Output = [T; 0];
-        fn pop_back(array: Self) -> (Self::Output, T) {
-            let [item,] = array;
-            ([], item)
-        }
-        fn pop_front(array: Self) -> (Self::Output, T) {
-            let [item,] = array;
-            ([], item)
-        }
-    }
-
-    impl<T> Sealed for [T; 1] {}
-    impl<T> ArrayTools for [T; 1] {
-        type Element = T;
-        const LEN: usize = 1;
-        fn as_slice(&self) -> &[Self::Element] { self }
-        fn as_mut_slice(&mut self) -> &mut [Self::Element] { self }
-
-        type Tuple = (T,);
-        fn from_tuple(tuple: Self::Tuple) -> Self {
-            let (a,) = tuple;
-            [a,]
-        }
-        fn into_tuple(self) -> Self::Tuple {
-            let [a,] = self;
-            (a,)
-        }
-    }
-    impl<T, U, F> ArrayMap<U, F> for [T; 1]
-        where F: FnOnce(T)->U
-    {
-        type Output = [U; 1];
-        fn map(array: Self, f: F) -> Self::Output {
-            let [a,] = array;
-            [f(a),]
-        }
-    }
-    impl<'a, T> ArrayAsRef for &'a [T; 1]
-    {
-        type Output = [&'a T; 1];
-        fn as_ref(array: Self) -> Self::Output {
-            let [a,] = array;
-            [a,]
-        }
-    }
-    impl<'a, T> ArrayAsMut for &'a mut [T; 1]
-    {
-        type Output = [&'a mut T; 1];
-        fn as_mut(array: Self) -> Self::Output {
-            let [a,] = array;
-            [a,]
-        }
-    }
-    impl<T> ArrayPush<T> for [T; 1] {
-        type Output = [T; 2];
-        fn push_back(array: Self, item: T) -> Self::Output {
-            let [a,] = array;
-            [a, item]
-        }
-        fn push_front(array: Self, item: T) -> Self::Output {
-            let [a,] = array;
-            [item, a,]
-        }
-    }
-    impl<T> ArrayPop<T> for [T; 2] {
-        type Output = [T; 1];
-        fn pop_back(array: Self) -> (Self::Output, T) {
-            let [a,item,] = array;
-            ([a,], item)
-        }
-        fn pop_front(array: Self) -> (Self::Output, T) {
-            let [item,a,] = array;
-            ([a,], item)
-        }
-    }
-
     macro_rules! replace_ident {
-        ($i:ident => $j:ident) => ($j)
+        ($i:ident => $($j:tt)*) => ($($j)*)
     }
     macro_rules! impl_for_size {
-        ($n:literal => $($i:ident)*) => (
+        ($n:literal $fn_trait:ident => $($i:ident)*) => (
 
             impl<T> Sealed for [T; $n] {}
             impl<T> ArrayTools for [T; $n] {
@@ -208,103 +112,112 @@ mod impls {
                 fn as_slice(&self) -> &[Self::Element] { self }
                 fn as_mut_slice(&mut self) -> &mut [Self::Element] { self }
 
-                type Tuple = ($(replace_ident!($i => T)),*);
+                type Tuple = ($(replace_ident!($i => T),)*);
                 fn from_tuple(tuple: Self::Tuple) -> Self {
-                    let ($($i),*) = tuple;
-                    [$($i),*]
+                    let ($($i,)*) = tuple;
+                    [$($i,)*]
                 }
                 fn into_tuple(self) -> Self::Tuple {
-                    let [$($i),*] = self;
-                    ($($i),*)
+                    let [$($i,)*] = self;
+                    ($($i,)*)
+                }
+            }
+            impl<T, F> ArrayGenerate<F> for [T; $n]
+                where F: $fn_trait() -> T
+            {
+                fn generate(mut f: F) -> Self {
+                    [$(replace_ident!($i => f()),)*]
                 }
             }
             impl<T, U, F> ArrayMap<U, F> for [T; $n]
-                where F: FnMut(T) -> U
+                where F: $fn_trait(T) -> U
             {
                 type Output = [U; $n];
                 fn map(array: Self, mut f: F) -> Self::Output {
-                    let [$($i),*] = array;
-                    [$(f($i)),*]
+                    let [$($i,)*] = array;
+                    [$(f($i),)*]
                 }
             }
             impl<'a, T> ArrayAsRef for &'a [T; $n]
             {
                 type Output = [&'a T; $n];
                 fn as_ref(array: Self) -> Self::Output {
-                    let [$($i),*] = array;
-                    [$($i),*]
+                    let [$($i,)*] = array;
+                    [$($i,)*]
                 }
             }
             impl<'a, T> ArrayAsMut for &'a mut [T; $n]
             {
                 type Output = [&'a mut T; $n];
                 fn as_mut(array: Self) -> Self::Output {
-                    let [$($i),*] = array;
-                    [$($i),*]
+                    let [$($i,)*] = array;
+                    [$($i,)*]
                 }
             }
             impl<T> ArrayPush<T> for [T; $n] {
                 type Output = [T; $n+1];
                 fn push_back(array: Self, item: T) -> Self::Output {
-                    let [$($i),*] = array;
-                    [$($i),*, item]
+                    let [$($i,)*] = array;
+                    [$($i,)* item]
                 }
                 fn push_front(array: Self, item: T) -> Self::Output {
-                    let [$($i),*] = array;
-                    [item, $($i),*]
+                    let [$($i,)*] = array;
+                    [item, $($i,)*]
                 }
             }
             impl<T> ArrayPop<T> for [T; $n+1] {
                 type Output = [T; $n];
                 fn pop_back(array: Self) -> (Self::Output, T) {
-                    let [$($i),*, item] = array;
-                    ([$($i),*], item)
+                    let [$($i,)* item] = array;
+                    ([$($i,)*], item)
                 }
                 fn pop_front(array: Self) -> (Self::Output, T) {
-                    let [item, $($i),*] = array;
-                    ([$($i),*], item)
+                    let [item, $($i,)*] = array;
+                    ([$($i,)*], item)
                 }
             }
 
         )
     }
 
+    impl_for_size!(0 FnOnce =>);
+    impl_for_size!(1 FnOnce => a0);
     // for i in 2..=32 {
-    //     print!("    impl_for_size!({} =>", i);
+    //     print!("    impl_for_size!({} FnMut =>", i);
     //     (0..i).for_each(|x| print!(" a{}", x));
     //     println!(");");
     // }
-    impl_for_size!(2 => a0 a1);
-    impl_for_size!(3 => a0 a1 a2);
-    impl_for_size!(4 => a0 a1 a2 a3);
-    impl_for_size!(5 => a0 a1 a2 a3 a4);
-    impl_for_size!(6 => a0 a1 a2 a3 a4 a5);
-    impl_for_size!(7 => a0 a1 a2 a3 a4 a5 a6);
-    impl_for_size!(8 => a0 a1 a2 a3 a4 a5 a6 a7);
-    impl_for_size!(9 => a0 a1 a2 a3 a4 a5 a6 a7 a8);
-    impl_for_size!(10 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9);
-    impl_for_size!(11 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10);
-    impl_for_size!(12 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11);
-    impl_for_size!(13 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12);
-    impl_for_size!(14 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13);
-    impl_for_size!(15 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14);
-    impl_for_size!(16 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15);
-    impl_for_size!(17 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16);
-    impl_for_size!(18 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17);
-    impl_for_size!(19 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18);
-    impl_for_size!(20 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19);
-    impl_for_size!(21 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20);
-    impl_for_size!(22 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21);
-    impl_for_size!(23 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22);
-    impl_for_size!(24 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23);
-    impl_for_size!(25 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24);
-    impl_for_size!(26 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25);
-    impl_for_size!(27 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26);
-    impl_for_size!(28 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27);
-    impl_for_size!(29 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28);
-    impl_for_size!(30 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29);
-    impl_for_size!(31 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30);
-    impl_for_size!(32 => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31);
+    impl_for_size!(2 FnMut => a0 a1);
+    impl_for_size!(3 FnMut => a0 a1 a2);
+    impl_for_size!(4 FnMut => a0 a1 a2 a3);
+    impl_for_size!(5 FnMut => a0 a1 a2 a3 a4);
+    impl_for_size!(6 FnMut => a0 a1 a2 a3 a4 a5);
+    impl_for_size!(7 FnMut => a0 a1 a2 a3 a4 a5 a6);
+    impl_for_size!(8 FnMut => a0 a1 a2 a3 a4 a5 a6 a7);
+    impl_for_size!(9 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8);
+    impl_for_size!(10 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9);
+    impl_for_size!(11 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10);
+    impl_for_size!(12 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11);
+    impl_for_size!(13 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12);
+    impl_for_size!(14 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13);
+    impl_for_size!(15 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14);
+    impl_for_size!(16 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15);
+    impl_for_size!(17 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16);
+    impl_for_size!(18 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17);
+    impl_for_size!(19 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18);
+    impl_for_size!(20 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19);
+    impl_for_size!(21 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20);
+    impl_for_size!(22 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21);
+    impl_for_size!(23 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22);
+    impl_for_size!(24 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23);
+    impl_for_size!(25 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24);
+    impl_for_size!(26 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25);
+    impl_for_size!(27 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26);
+    impl_for_size!(28 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27);
+    impl_for_size!(29 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28);
+    impl_for_size!(30 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29);
+    impl_for_size!(31 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30);
+    impl_for_size!(32 FnMut => a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31);
 }
 
 #[cfg(test)]
